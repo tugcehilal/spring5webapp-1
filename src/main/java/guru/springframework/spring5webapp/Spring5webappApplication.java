7ac3150a6a17bf7c1e6acf09329b2b1f768d08ee
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-18 18:09:46 +0300

    Revert "test commit"
    This reverts commit 960b1b4be847e4a4db82b1d3e28ccb5242ddbb01.

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-
-
-   //test tugce son
-
+	git
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
