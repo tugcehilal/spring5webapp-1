3185c2d7326530c6effdbdf4d27920e1ebf3929b
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-18 18:34:13 +0300

    Revert "creating of hibernate tables"
    This reverts commit f4a156109beb8c201e0327994f39a865727d5174.

--- src/main/java/guru/springframework/spring5webapp/respositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.spring5webapp.respositories;
-
-import guru.springframework.spring5webapp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
--- src/main/java/guru/springframework/spring5webapp/respositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.spring5webapp.respositories;
-
-import guru.springframework.spring5webapp.domain.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository <Book, Long> {
-}
