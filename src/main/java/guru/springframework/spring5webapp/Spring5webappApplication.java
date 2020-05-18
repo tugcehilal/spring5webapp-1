56c1c12761c67913a97a34d567c2618d839a81a8
Tugce Hilal <tugce.hilal.cil@gmail.com>
2020-05-18 18:01:34 +0300

    solved conflict

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5webappApplication {
-   //test tugce
-	//test tugce2
-	//test tugce bugun benim dogum gunum.
+
+   //test tugce son
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
